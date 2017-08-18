0345bcc3ced41f6e6bdd53cf363e396d19c5d4b2
James Matsumura <james.matsumura@gmail.com>
2017-08-18 13:30:03 -0500

    Adding an ignore file

--- conf.py
+++
@@ -1,8 +0,0 @@
-app_root = '/path/to/app'
-access_origin = 'localhost'
-be_port = '80'
-neo4j_ip = '111.111.111.11'
-neo4j_bolt = '7687'
-neo4j_http = '7474'
-neo4j_un = 'username'
-neo4j_pw = 'password'
