a323959f2de3ca592c027010d03659fa117211d3
James Matsumura <james.matsumura@gmail.com>
2017-09-06 13:40:04 -0500

    Correcting how file type hits BE

--- query.py
+++ query.py
@@ -1023,6 +1023,7 @@ def convert_portal_to_neo4j(inp_str):
     if "F." not in inp_str:
         ***REMOVED***File
         inp_str = inp_str.replace("file.","F.")
+        inp_str = inp_str.replace("F.type","F.node_type")
         inp_str = inp_str.replace("File_","F.")
 
     if "T." not in inp_str:
